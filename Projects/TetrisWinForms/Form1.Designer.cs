a1534457791eb52b044348faa6bae06965dc91e1
Admin <Admin@DESKTOP-K078S8T>
2026-03-29 14:44:10 +0800

    ФИО

--- Projects/TetrisWinForms/Form1.Designer.cs
+++ Projects/TetrisWinForms/Form1.Designer.cs
@@ -3,7 +3,6 @@ namespace TetrisWinForms;
 partial class Form1
 {
     private System.ComponentModel.IContainer components = null;
-    private Label labelTitle;
     private Label labelAuthor;
 
     protected override void Dispose(bool disposing)
@@ -18,40 +17,31 @@ partial class Form1
 
     private void InitializeComponent()
     {
-        components = new System.ComponentModel.Container();
-        labelTitle = new Label();
         labelAuthor = new Label();
         SuspendLayout();
-
+        // 
+        // labelAuthor
+        // 
+        labelAuthor.AutoSize = true;
+        labelAuthor.Font = new Font("Segoe UI", 16F);
+        labelAuthor.ForeColor = Color.FromArgb(73, 80, 87);
+        labelAuthor.Location = new Point(12, 18);
+        labelAuthor.Name = "labelAuthor";
+        labelAuthor.Size = new Size(394, 30);
+        labelAuthor.TabIndex = 1;
+        labelAuthor.Text = "Автор: Благодатских Антон Иванович";
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(8F, 20F);
         AutoScaleMode = AutoScaleMode.Font;
         BackColor = Color.FromArgb(245, 247, 250);
         ClientSize = new Size(640, 360);
-        Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+        Controls.Add(labelAuthor);
+        Font = new Font("Segoe UI", 11F);
+        Name = "Form1";
         StartPosition = FormStartPosition.CenterScreen;
         Text = "Лабораторная работа №1";
-
-        labelTitle.AutoSize = true;
-        labelTitle.Font = new Font("Segoe UI Semibold", 24F, FontStyle.Bold, GraphicsUnit.Point);
-        labelTitle.ForeColor = Color.FromArgb(33, 37, 41);
-        labelTitle.Location = new Point(78, 84);
-        labelTitle.Name = "labelTitle";
-        labelTitle.Size = new Size(249, 54);
-        labelTitle.TabIndex = 0;
-        labelTitle.Text = "Tetris WinForms";
-
-        labelAuthor.AutoSize = true;
-        labelAuthor.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
-        labelAuthor.ForeColor = Color.FromArgb(73, 80, 87);
-        labelAuthor.Location = new Point(78, 156);
-        labelAuthor.Name = "labelAuthor";
-        labelAuthor.Size = new Size(458, 37);
-        labelAuthor.TabIndex = 1;
-        labelAuthor.Text = "Автор: Благодатских Антон Иванович";
-
-        Controls.Add(labelAuthor);
-        Controls.Add(labelTitle);
-        Name = "Form1";
-
         ResumeLayout(false);
         PerformLayout();
     }
